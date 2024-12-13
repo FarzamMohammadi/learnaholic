4880729f47b3bd325d5f7f62e7dea6b9230edfc0
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2024-12-12 19:11:28 -0500

    Remove CacheLevel & ItemLevel TTL options from cache policy

--- Coding-Problems/CSharp/EnhancedLRUCache/LruPolicy.cs
+++ Coding-Problems/CSharp/EnhancedLRUCache/LruPolicy.cs
@@ -1,22 +1,18 @@
 ﻿namespace EnhancedLRUCache;
 
-[Flags]
 public enum TtlPolicy
 {
     None = 0,
-    AbsoluteExpiration = 1 << 0, // 0001 - 1 - Items expire at a specific datetime
-    SlidingExpiration = 1 << 1,  // 0010 - 2 - Items expire after period of non-use
-    CacheLevelTtl = 1 << 2,      // 0100 - 4 - TTL settings applied at cache level
-    ItemLevelTtl = 1 << 3        // 1000 - 8 - TTL settings applied at individual item level
+    Absolute = 1, // Items expire at a specific datetime
+    Sliding = 2   // Items expire after period of non-use
 }
 
 public interface ILruPolicy
 {
     public TimeSpan? DefaultTtl { get; }
+
     public bool UsesAbsoluteExpiration { get; }
     public bool UsesSlidingExpiration { get; }
-    public bool UsesCacheLevelTtl { get; }
-    public bool UsesItemLevelTtl { get; }
 }
 
 public class LruPolicy
@@ -27,8 +23,6 @@ public class LruPolicy
 {
     public TimeSpan? DefaultTtl { get; } = defaultTtl;
 
-    public bool UsesAbsoluteExpiration => policy.HasFlag(TtlPolicy.AbsoluteExpiration);
-    public bool UsesSlidingExpiration => policy.HasFlag(TtlPolicy.SlidingExpiration);
-    public bool UsesCacheLevelTtl => policy.HasFlag(TtlPolicy.CacheLevelTtl);
-    public bool UsesItemLevelTtl => policy.HasFlag(TtlPolicy.ItemLevelTtl);
+    public bool UsesAbsoluteExpiration => policy.HasFlag(TtlPolicy.Absolute);
+    public bool UsesSlidingExpiration => policy.HasFlag(TtlPolicy.Sliding);
 }
